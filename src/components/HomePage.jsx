a1aa98224e994150761454baddfe3f720a3275f3
dandybytes <dandybytes@gmx.com>
2020-12-08 21:59:40 +0100

    Create dummy home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
+import {Divider, Grid, Image, Segment, Header, Card, Container} from 'semantic-ui-react'
+
 const HomePage = () => {
     return (
-        <div>
-            <p>Home page here</p>
-        </div>
+        <Container>
+            <Segment>
+                <Grid columns={2} stackable textAlign="center">
+                    <Divider vertical>Or</Divider>
+
+                    <Grid.Row verticalAlign="middle">
+                        <Grid.Column textAlign="center">
+                            <Card
+                                header="Topic Modelling"
+                                meta="NLP"
+                                description="Using statistical modelling to discover topics in a textual collection."
+                                href="/topic-modelling"
+                                style={{margin: '0 auto'}}
+                            />
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Card
+                                header="Geolocation"
+                                meta="geolocation"
+                                description="Methods of geolocation which can be implemented iteratively to produce standardized coordinate data"
+                                href="/"
+                                style={{margin: '0 auto'}}
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+        </Container>
     )
 }
 
