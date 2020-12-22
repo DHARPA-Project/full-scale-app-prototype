7cde800da41ca6f372709c993f5ae43a80e53346
dandybytes <dandybytes@gmx.com>
2020-12-22 22:05:57 +0100

    Install framer-motion; animate Modal

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -144,21 +144,17 @@ const FileUpload = () => {
                     </Grid>
                 </form>
             </Segment>
-            {showModal && (
-                <Modal showCross={false} message={''} closeModal={() => setShowModal(false)}>
-                    <h1>File Upload Instructions</h1>
-                    <ul>
-                        <li>Only *.txt files can be submitted for topic modelling analysis</li>
-                        <li>
-                            The file names must contain a time stamp of the following format: *#%@*
-                        </li>
-                        <li>
-                            The file names or content may not include discrediting information about
-                            Italy or Italians (for your own sake)
-                        </li>
-                    </ul>
-                </Modal>
-            )}
+            <Modal showCross={false} isVisible={showModal} setIsVisible={setShowModal}>
+                <h1>File Upload Instructions</h1>
+                <ul>
+                    <li>Only *.txt files can be submitted for topic modelling analysis</li>
+                    <li>The file names must contain a time stamp of the following format: *#%@*</li>
+                    <li>
+                        The file names or content may not include discrediting information about
+                        Italy or Italians (for your own sake)
+                    </li>
+                </ul>
+            </Modal>
         </React.Fragment>
     )
 }
--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,33 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {AnimatePresence, motion} from 'framer-motion'
+
 import './Modal.scss'
 
-const Modal = ({showCross, message, children, closeModal}) => {
+const Modal = ({showCross, message, children, isVisible, setIsVisible}) => {
     return (
-        <div className="modal-backdrop" onClick={() => closeModal()}>
-            <div className="modal-content">
-                <span className="modal-close" style={{display: showCross ? 'block' : 'none'}}>
-                    &times;
-                </span>
-                {message && <h2 className="modal-title">{message}</h2>}
-                {children}
-            </div>
-        </div>
+        <AnimatePresence>
+            {isVisible && (
+                <motion.div
+                    className="modal-backdrop"
+                    onClick={() => setIsVisible(false)}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1, transition: {duration: 0.3}}}
+                    exit={{opacity: 0, transition: {delay: 0.3}}}
+                >
+                    <motion.div
+                        className="modal-container"
+                        initial={{scale: 0}}
+                        animate={{scale: 1, transition: {duration: 0.3}}}
+                        exit={{scale: 0, transition: {delay: 0.3}}}
+                    >
+                        <span
+                            className="modal-close"
+                            style={{display: showCross ? 'block' : 'none'}}
+                        >
+                            &times;
+                        </span>
+                        <motion.div
+                            className="modal-content"
+                            initial={{x: 100, opacity: 0}}
+                            animate={{x: 0, opacity: 1, transition: {delay: 0.3, duration: 0.3}}}
+                            exit={{x: 100, opacity: 0, transition: {duration: 0.3}}}
+                        >
+                            {message && <h2 className="modal-title">{message}</h2>}
+                            {children}
+                        </motion.div>
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     )
 }
 
 Modal.propTypes = {
     showCross: PropTypes.bool,
+    message: PropTypes.string,
     className: PropTypes.string,
-    style: PropTypes.object,
-    closeModal: PropTypes.func.isRequired
+    isVisible: PropTypes.bool,
+    setIsVisible: PropTypes.func.isRequired
 }
 
 Modal.defaultProps = {
     showCross: true,
-    message: 'Are you sure?',
+    message: '',
     className: 'Modal',
-    closeModal: () => console.error('closeModal() function argument missing!')
+    isVisible: false,
+    setIsVisible: () => console.error('setIsVisible() function argument missing!')
 }
 
 export default Modal
