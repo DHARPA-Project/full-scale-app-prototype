30d85328e575e7bb46b11179160ee09b26371b5a
dandybytes <dandybytes@gmx.com>
2020-12-29 18:36:36 +0100

    Make text route retrieve sample text by pool ID

--- backend/routes/textRoute.js
+++ backend/routes/textRoute.js
@@ -1,16 +1,25 @@
-import express from 'express'
+import express, {text} from 'express'
 
 const router = express.Router()
 
 import {removeHtml} from '../utils/textCleanup.js'
 
-import {sample1} from '../data/sample.js'
+import sampleText from '../data/sample.js'
 
-router.route('/processing').post((req, res) => {
-    const original = sample1
-    const processed = removeHtml(sample1)
+router.route('/processing').get((req, res) => {
+    console.log(req.query)
+    const {id, operations} = req.query
+    const textPool = sampleText.find(item => item.id === id)
 
-    return res.json({success: true, original, processed})
+    if (!textPool) {
+        res.status(404)
+        return res.send({success: false, error: 'text pool with specified ID not found'})
+    }
+
+    const original = textPool.text
+    const processed = removeHtml(original)
+
+    return res.status(200).json({success: true, original, processed})
 })
 
 export default router
