ef0ae20f2e519fbbfc929a817daac49f608e2d43
dandybytes <dandybytes@gmx.com>
2020-12-29 21:46:56 +0100

    Text processing utility & route

--- backend/routes/textRoute.js
+++ backend/routes/textRoute.js
@@ -2,24 +2,29 @@ import express, {text} from 'express'
 
 const router = express.Router()
 
-import {removeHtml} from '../utils/textCleanup.js'
+import {processText} from '../utils/textCleanup.js'
 
 import sampleText from '../data/sample.js'
 
 router.route('/processing').get((req, res) => {
-    console.log(req.query)
-    const {id, operations} = req.query
-    const textPool = sampleText.find(item => item.id === id)
+    try {
+        const {id, operations} = req.query
+        const textPool = sampleText.find(item => item.id === id)
 
-    if (!textPool) {
-        res.status(404)
-        return res.send({success: false, error: 'text pool with specified ID not found'})
+        if (!textPool) {
+            res.status(404)
+            return res.json({success: false, error: 'text pool with specified ID not found'})
+        }
+
+        const original = textPool.text
+        const textOperations = typeof operations === 'text' ? [operations] : operations
+        const processed = processText(original, textOperations)
+
+        return res.status(200).json({success: true, original, processed})
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({success: false, error: 'Internal server error'})
     }
-
-    const original = textPool.text
-    const processed = removeHtml(original)
-
-    return res.status(200).json({success: true, original, processed})
 })
 
 export default router
--- backend/utils/textCleanup.js
+++ backend/utils/textCleanup.js
@@ -1,29 +1,118 @@
-const regexFragments = {
-    html: '<[^>]+>',
-    numbers: `0-9`,
-    punctuation: `\.!\?,;:\-'"()`,
-    special: `\/#$%\^&\*{}=_\`~@\+><\[\]`,
-    twoLetterWords: `[a-zA-Z]{2}`,
-    threeLetterWords: `[a-zA-Z]{3}`
+const allowedOperations = {
+    html: 'html',
+    alphanumerical: 'alphanumerical',
+    alphabetical: 'alphabetical',
+    numbers: 'numbers',
+    punctuation: 'punctuation',
+    special: 'special',
+    short: 'short',
+    lowercase: 'lowercase'
 }
 
-// \w is any digit, letter, or underscore.
-// \s is any whitespace.
-// [^\w\s] is anything that's not a digit, letter, whitespace, or underscore.
-// [^\w\s]|_ removes the underscores from the above RegEx
-const extractOnlyAlphabetical = text => text.replace(/[^\w\s]|_/g, '')
+const regexFragments = {
+    html: '<[^>]+>',
+    numbers: `[0-9]`,
+    // \w is any digit, letter, or underscore.
+    // \s is any whitespace.
+    // [^\w\s] is anything that's not a digit, letter, whitespace, or underscore.
+    // [^\w\s]|_ removes the underscores from the above RegEx
+    // backslashes must be escaped to prevent them from being eliminated when strings are parsed
+    alphanumerical: '[^\\w\\s]|_',
+    alphabetical: '[^a-zA-Z\\s]',
+    punctuation: `[()'"’“”\\.!\\?,;:\\-]`,
+    special: '[\\/#$%\\^&\\*{}=_\\`~@\\+><\\[\\]]',
+    twoLetterWords: `\\b[a-zA-Z]{2}\\b`,
+    threeLetterWords: `\\b[a-zA-Z]{3}\\b`
+}
 
-// replace all newline characters with a space...
-// ...and reduce multiple whitespace characters to a single blank space
-export const trimWhitespaces = text => text.replace(/\s+/g, ' ')
+const getOnlyAlphanumerical = text =>
+    text.replace(new RegExp(regexFragments.alphanumerical, 'g'), '')
 
-export const removeHtml = text => text.replace(new RegExp(regexFragments.html, 'g'), '')
+const getOnlyAlphabetical = text => text.replace(new RegExp(regexFragments.alphabetical, 'g'), '')
 
-export const removeNumbers = text => text.replace(/[0-9]/g, '')
+const removeHtml = text => text.replace(new RegExp(regexFragments.html, 'g'), '')
 
-export const removePunctuation = text =>
-    text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '')
+const removeNumbers = text => text.replace(new RegExp(regexFragments.numbers, 'g'), '')
 
-export const removeSpecialChars = text => text.replace(/[0-9]/g, '')
+const removePunctuation = text => text.replace(new RegExp(regexFragments.punctuation, 'g'), '')
 
-export const removeShortWords = text => text.replace(/[0-9]/g, '')
+const removeSpecialChars = text => text.replace(new RegExp(regexFragments.special, 'g'), '')
+
+const removeShortWords = text => text.replace(new RegExp(regexFragments.twoLetterWords, 'g'), '')
+
+// text processing operations must be performed in a certain logical order
+// give the order of operations careful consideration before changing it!
+export const processText = (text, operations) => {
+    const {
+        html,
+        alphabetical,
+        alphanumerical,
+        numbers,
+        punctuation,
+        special,
+        short,
+        lowercase
+    } = allowedOperations
+    let remainingOperations = [...operations]
+    let output = text
+
+    // TO DO: clean up HTML entities / handle text encoding
+    // html tags must be removed before any other operation is performed
+    if (remainingOperations.includes(html)) {
+        output = removeHtml(output)
+        remainingOperations = remainingOperations.filter(operation => operation !== 'html')
+    }
+
+    // if the user wants to keep only alphanumerical chars, then numbers and all special...
+    // ... characters are elemminated by default
+    if (remainingOperations.includes(alphanumerical)) {
+        output = getOnlyAlphanumerical(output)
+        remainingOperations = remainingOperations.filter(
+            operation =>
+                ![alphabetical, alphanumerical, numbers, punctuation, special].includes(operation)
+        )
+    }
+
+    // if the user wants to keep only alphabetical chars, then numbers and all special...
+    // ... characters are elemminated by default
+    if (remainingOperations.includes(alphabetical)) {
+        output = getOnlyAlphabetical(output)
+        remainingOperations = remainingOperations.filter(
+            operation =>
+                ![alphabetical, alphanumerical, numbers, punctuation, special].includes(operation)
+        )
+    }
+
+    // if the user did not choose to keep only alphabetical or alphanumerical characters...
+    // ... but chose to eliminate a specific range of characters:
+    const tier2 = remainingOperations.filter(operation =>
+        [numbers, punctuation, special].includes(operation)
+    )
+    if (tier2.length) {
+        const regex = new RegExp(
+            tier2.map(operationType => regexFragments[operationType]).join('|'),
+            'g'
+        )
+        output = output.replace(regex, '')
+        remainingOperations = remainingOperations.filter(
+            operation => ![numbers, punctuation, special].includes(operation)
+        )
+    }
+
+    // short words should be dealt with only after eliminating all special character groups
+    if (remainingOperations.includes(short)) {
+        output = removeShortWords(output)
+        remainingOperations = remainingOperations.filter(operation => operation !== short)
+    }
+
+    // replace all newline characters with a space...
+    // ...and reduce multiple whitespace characters to a single blank space
+    output = output.replace(/\s+/g, ' ')
+
+    // lowercasing must be done only after all the char removal operations have been performed
+    if (remainingOperations.includes(lowercase)) {
+        output = output.toLowerCase()
+    }
+
+    return output
+}
