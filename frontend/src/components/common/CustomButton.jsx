d49d20a6eff116016b6f5843138ca3eada0c396a
dandybytes <dandybytes@gmx.com>
2020-12-30 23:21:01 +0100

    Create CustomButton component

---
+++ frontend/src/components/common/CustomButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './CustomButton.scss'
+
+const CustomButton = ({children, inverted, ...otherProps}) => {
+    return (
+        <button className={`${inverted ? 'inverted ' : ''}custom-button`} {...otherProps}>
+            {children}
+        </button>
+    )
+}
+
+CustomButton.propTypes = {
+    inverted: PropTypes.bool
+}
+
+CustomButton.defaultProps = {
+    className: 'custom-button'
+}
+
+export default CustomButton
