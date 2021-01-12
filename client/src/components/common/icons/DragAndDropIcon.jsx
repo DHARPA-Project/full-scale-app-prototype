7ff23aabd9c65c5a0642330f3881c552afcb0b75
dandybytes <dandybytes@gmx.com>
2021-01-12 17:31:38 +0100

    Adjust drag-n-drop icon to take style through classes

--- client/src/components/common/icons/DragAndDropIcon.jsx
+++ client/src/components/common/icons/DragAndDropIcon.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
-import {IconContext} from 'react-icons'
 import {RiDragDropFill} from 'react-icons/ri'
 
-const DragAndDropIcon = () => {
-    return (
-        <IconContext.Provider value={{color: 'rgba(0, 0, 0, 0.87)'}}>
-            <div>
-                <RiDragDropFill className="icon" />
-            </div>
-        </IconContext.Provider>
-    )
+const DragAndDropIcon = ({classes}) => {
+    return <RiDragDropFill className={`icon${classes ? ` ${classes}` : ''}`} />
 }
 
 export default DragAndDropIcon
