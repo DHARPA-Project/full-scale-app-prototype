1cc1d39248a126825c6ac3957788e9a363364a52
dandybytes <dandybytes@gmx.com>
2021-01-13 17:39:17 +0100

    FileList: custom style + integrate sub-components

--- client/src/components/FileList.jsx
+++ client/src/components/FileList.jsx
@@ -1,18 +1,19 @@
 import React, {useContext, useState, useEffect} from 'react'
 
-import {Button, Icon, Label, Segment, Table} from 'semantic-ui-react'
+import {FaTrashAlt} from 'react-icons/fa'
 
-import {Context} from '../context'
 import FileListPlaceholder from './FileListPlaceholder'
-import {fileTypes} from '../constants/const'
+import CustomButton from './common/CustomButton'
 
 import './FileList.scss'
+import {Context} from '../context'
+import {fileTypes} from '../constants/const'
+import FileTable from './FileTable'
 
 const FileList = () => {
     const {
         uploadedFiles,
         setUploadedFiles,
-        removeUploadedFileById,
         removeAllInvalidFiles,
         setFilesReadyForSubmission
     } = useContext(Context)
@@ -29,90 +30,27 @@ const FileList = () => {
 
     return (
         <div className="file-list">
-            <Segment style={{display: 'flex'}}>
-                <Button
-                    className="remove-files"
-                    icon
-                    labelPosition="left"
-                    size="small"
-                    onClick={() => setUploadedFiles([])}
-                >
-                    <Icon name="trash alternate" /> Remove all files
-                </Button>
-                <div className="stats" style={{margin: 'auto'}}>
-                    <Label color="green" horizontal>
-                        <span className="stats">{numValidFiles} valid</span>
-                    </Label>
+            <FileTable />
+
+            <div className="file-list-control-options">
+                <CustomButton onClick={() => setUploadedFiles([])}>
+                    <FaTrashAlt />
+                    &nbsp;&nbsp;Remove all files
+                </CustomButton>
+
+                <div className="stats">
+                    <div className="stats-bubble valid">{numValidFiles} valid</div>
                     {numValidFiles < uploadedFiles.length && (
-                        <Label color="red" horizontal>
-                            <span className="stats">
-                                {uploadedFiles.length - numValidFiles} invalid
-                            </span>
-                        </Label>
+                        <span className="stats-bubble invalid">
+                            {uploadedFiles.length - numValidFiles} invalid
+                        </span>
                     )}
                 </div>
-                <Button
-                    className="remove-files"
-                    icon
-                    labelPosition="left"
-                    size="small"
-                    onClick={removeAllInvalidFiles}
-                >
-                    <Icon name="trash alternate" /> Remove invalid files
-                </Button>
-            </Segment>
-            <Table celled compact definition>
-                <Table.Header fullWidth>
-                    <Table.Row>
-                        <Table.HeaderCell textAlign="center">Name</Table.HeaderCell>
-                        <Table.HeaderCell textAlign="center">Size</Table.HeaderCell>
-                        <Table.HeaderCell textAlign="center">Date</Table.HeaderCell>
-                        <Table.HeaderCell textAlign="center">Valid</Table.HeaderCell>
-                        <Table.HeaderCell></Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-
-                <Table.Body>
-                    {uploadedFiles.map(file => {
-                        const isValidFile = file.fileObj.type === fileTypes.text
-                        return (
-                            <Table.Row key={file.id} negative={!isValidFile}>
-                                <Table.Cell style={{paddingLeft: '2rem'}}>
-                                    {isValidFile ? (
-                                        <Icon name="file outline" />
-                                    ) : (
-                                        <Icon name="question" color="red" />
-                                    )}
-                                    &nbsp;&nbsp;
-                                    {file.fileObj.name}
-                                </Table.Cell>
-                                <Table.Cell textAlign="center">{file.fileObj.size}</Table.Cell>
-                                <Table.Cell textAlign="center">
-                                    {file.fileObj.lastModified}
-                                </Table.Cell>
-                                <Table.Cell textAlign="center">
-                                    {isValidFile ? (
-                                        <Icon color="green" name="checkmark" size="large" />
-                                    ) : (
-                                        <Icon color="red" name="dont" size="large" />
-                                    )}
-                                </Table.Cell>
-                                <Table.Cell collapsing>
-                                    <Button
-                                        animated="vertical"
-                                        onClick={() => removeUploadedFileById(file.id)}
-                                    >
-                                        <Button.Content hidden>Remove</Button.Content>
-                                        <Button.Content visible>
-                                            <Icon name="trash alternate" />
-                                        </Button.Content>
-                                    </Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
-            </Table>
+                <CustomButton onClick={removeAllInvalidFiles}>
+                    <FaTrashAlt />
+                    &nbsp;&nbsp;Remove invalid files
+                </CustomButton>
+            </div>
         </div>
     )
 }
