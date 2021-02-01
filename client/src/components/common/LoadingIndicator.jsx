44b23cd8fbe757f6a4ecfa323fe238b5202c7d7f
dandybytes <dandybytes@gmx.com>
2021-02-01 14:36:41 +0100

    Create responsive loading indicator

---
+++ client/src/components/common/LoadingIndicator.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './LoadingIndicator.scss'
+
+const LoadingIndicator = () => {
+    return (
+        <div className="loading-indicator">
+            <div className="loading-indicator-content">
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+            </div>
+        </div>
+    )
+}
+
+export default LoadingIndicator
