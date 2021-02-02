d19dec63cfc0e5de28a786a09674d8664c7dffff
dandybytes <dandybytes@gmx.com>
2021-02-02 21:36:11 +0100

    Create PageHeader component

---
+++ client/src/components/common/PageHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './PageHeader.scss'
+
+const PageHeader = ({children}) => {
+    return <div className="page-header">{children}</div>
+}
+
+export default PageHeader
