891584d73ce384650658ecc042df4fb2837b5dad
dandybytes <dandybytes@gmx.com>
2021-02-02 12:37:47 +0100

    Update FileBatchTable

--- client/src/components/FileBatchTable.jsx
+++ client/src/components/FileBatchTable.jsx
@@ -1,23 +1,22 @@
 import React, {useContext, useEffect, useState} from 'react'
 import axios from 'axios'
 
-import {FaTrashAlt} from 'react-icons/fa'
-
 import './FileBatchTable.scss'
 import {Context} from '../context'
 
-import Spinner from './common/Spinner'
-import {Link} from 'react-router-dom'
+import LoadingIndicator from './common/LoadingIndicator'
+import FileBatchRow from './FileBatchRow'
 
 const FileBatchTable = () => {
     const {loggedInUser, createNotification} = useContext(Context)
 
-    const [fileBatchLoading, setFileBatchLoading] = useState(false)
+    const [fileBatchesLoading, setFileBatchesLoading] = useState(false)
     const [fileBatches, setFileBatches] = useState([])
 
     useEffect(() => {
         const fetchFileBatches = async () => {
-            setFileBatchLoading(true)
+            setFileBatchesLoading(true)
+
             try {
                 const response = await axios.get('/api/data', {
                     headers: {Authorization: 'Bearer ' + loggedInUser.token}
@@ -26,28 +25,26 @@ const FileBatchTable = () => {
                 const {success, error, batches} = response.data
 
                 if (success) {
+                    setFileBatchesLoading(false)
                     setFileBatches(batches)
                 } else {
                     throw new Error(error)
                 }
             } catch (error) {
+                setFileBatchesLoading(false)
                 createNotification(
                     `This page failed to load, because required data could not be retrieved from the server. ${error}`, //message
                     'error', // type
                     0 // setting duration to 0 will make it never expire
                 )
                 console.error('Failed to fetch file batch data from the server:\n', error)
-            } finally {
-                setFileBatchLoading(false)
             }
         }
 
         fetchFileBatches()
+    }, [createNotification, loggedInUser.token])
 
-        // eslint-disable-next-line
-    }, [])
-
-    if (fileBatchLoading || !fileBatches) return <Spinner />
+    if (fileBatchesLoading || !fileBatches) return <LoadingIndicator />
 
     return (
         <table className="file-batch-table">
@@ -57,34 +54,24 @@ const FileBatchTable = () => {
                     <th>Created</th>
                     <th>Tags</th>
                     <th>Files</th>
-                    <th>Processing</th>
+                    <th>Options</th>
                     <th>Output</th>
-                    <th></th>
+                    <th>...</th>
                 </tr>
             </thead>
 
             <tbody>
                 {fileBatches.map(batch => {
-                    const {id, title, date, tags, files, options} = batch
                     return (
-                        <tr key={id}>
-                            <td>{title}</td>
-                            <td>{date.split('T')[0]}</td>
-                            <td>{tags}</td>
-                            <td>{files}</td>
-                            <td>{options}</td>
-                            <td>
-                                <Link to={`/output/${id}`}>see output</Link>
-                            </td>
-                            <td>
-                                <div
-                                    className="remove-file-button"
-                                    onClick={() => console.log('removing file batch with ID ', id)}
-                                >
-                                    <FaTrashAlt />
-                                </div>
-                            </td>
-                        </tr>
+                        <FileBatchRow
+                            key={batch.id}
+                            {...batch}
+                            removeFileBatch={id =>
+                                setFileBatches(prevFileBatches =>
+                                    prevFileBatches.filter(batch => batch.id !== id)
+                                )
+                            }
+                        />
                     )
                 })}
             </tbody>
