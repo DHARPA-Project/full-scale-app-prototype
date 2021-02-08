bb90bd27d8099cb4122f620412aed1079aeb8be3
dandybytes <dandybytes@gmx.com>
2021-02-08 20:59:09 +0100

    Add page header to file management page

--- client/src/components/pages/FileManagementPage.jsx
+++ client/src/components/pages/FileManagementPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import PageWrapper from '../common/PageWrapper'
+import PageHeader from '../common/PageHeader'
 import FileBatchTable from '../FileBatchTable'
 
 import './FileManagementPage.scss'
@@ -9,7 +10,9 @@ const FileManagementPage = () => {
     return (
         <PageWrapper>
             <div className="file-management-page">
-                <h1 className="file-management-heading">Manage your files</h1>
+                <PageHeader>
+                    <h1 className="file-management-title">Manage your files</h1>
+                </PageHeader>
                 <div className="file-management-container">
                     <FileBatchTable />
                 </div>
