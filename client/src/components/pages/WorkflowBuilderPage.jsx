d3d316bbfdaeb51c09d367707663e7d58b924480
dandybytes <dandybytes@gmx.com>
2021-02-15 14:51:52 +0100

    Eliminate unnecessary header & reduce wasted space

--- client/src/components/pages/WorkflowBuilderPage.jsx
+++ client/src/components/pages/WorkflowBuilderPage.jsx
@@ -3,19 +3,12 @@ import React from 'react'
 import './WorkflowBuilderPage.scss'
 
 import PageWrapper from '../common/PageWrapper'
-import PageHeader from '../common/PageHeader'
 import ModuleBoard from '../ModuleBoard'
 
 const WorkflowBuilderPage = () => {
     return (
         <PageWrapper>
             <div className="workflow-composition-page">
-                <PageHeader>
-                    <h1 className="workflow-composition-title">
-                        <span>Compose your Workflow</span>
-                    </h1>
-                </PageHeader>
-
                 <div className="workflow-composition-content">
                     <ModuleBoard />
                 </div>
