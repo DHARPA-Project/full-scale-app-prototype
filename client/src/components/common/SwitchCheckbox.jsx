97effe4acd8577f411eb3276219f67d9ac105048
dandybytes <dandybytes@gmx.com>
2021-02-16 14:48:48 +0100

    Refactor SwitchCheckbox to accommodate text label

--- client/src/components/common/SwitchCheckbox.jsx
+++ client/src/components/common/SwitchCheckbox.jsx
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types'
 
 import './SwitchCheckbox.scss'
 
-const SwitchCheckbox = ({enabled, classes, onToggle}) => {
+const SwitchCheckbox = ({enabled, classes, onToggle, label, value}) => {
     return (
-        <label className={`switch ${classes}`}>
-            <input
-                type="checkbox"
-                className="switch-checkbox"
-                checked={enabled}
-                onChange={onToggle}
-            />
-            <span className="switch-slider"></span>
+        <label className={`switch-label ${classes}`}>
+            <div className={`switch-container`}>
+                <input
+                    type="checkbox"
+                    className="switch-checkbox"
+                    checked={enabled}
+                    value={value}
+                    onChange={onToggle}
+                />
+                <span className="switch-slider"></span>
+            </div>
+            <span className="switch-label">{label}</span>
         </label>
     )
 }
@@ -20,6 +24,7 @@ const SwitchCheckbox = ({enabled, classes, onToggle}) => {
 SwitchCheckbox.propTypes = {
     enabled: PropTypes.bool,
     classes: PropTypes.string,
+    value: PropTypes.string,
     onToggle: PropTypes.func.isRequired
 }
 
